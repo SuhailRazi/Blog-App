00ca9afb180d482c08e465599c6872417be8cbb7
Suhail Razi P M <suhailrazij@gmail.com>
2023-07-17 22:42:13 +0530

    Added middleware endpoints for auth user post

--- BackendServer/index.js
+++ BackendServer/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import postRoutes from "./routes/post.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+
 
 const port = 8800
 const app = express();
 
 app.use(express.json());
 
+app.use("api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 
 app.listen(port,()=>{
